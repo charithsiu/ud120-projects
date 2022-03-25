8c36112ae43714ee8a3dd23961f2d96be17afad4
Charith Atapattu <31107032+charithsiu@users.noreply.github.com>
2022-03-25 16:23:10 -0500

    Quiz3 done

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,8 +26,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+t0 = time()
+clf = clf.fit(features_train,labels_train)
+print("Testing Time:", round(time()-t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+print(len(features_train[0]))
 #########################################################
 
 
